debae0429850f5dbd5db99bc4578598dd3e650d8
Petia Valchkova <peps@gmail.com>
2022-06-24 22:30:56 +0300

    template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero/Hero';
+/*import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+  AuthWrapper,
+} from './pages';*/
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
- 
+     {/*<AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:id' element={<SingleProduct />} />
+          <Route
+            path='checkout'
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path='error' element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+          </AuthWrapper>*/}
     </>
   );
 }
--- src/Hero/Products.js
+++ src/Hero/Products.js
@@ -1,24 +1,25 @@
 import data from '../data/data'
 function Products() {
-       return (
+    return (
         <div> <h1>Poduct</h1>;
-        <div className='products'>
-          {data.products.map((product) => (
-            <div className='product' key={product.slug}>
-              <a href={'/product/${product.slug}'}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className='product-info'>
-                <a href={'/product/${product.slug}'}>
-                  <p>{product.name}</p>
-                </a>
-                <p><strong>${product.price}</strong></p>
-                <button>Add to cart</button>
-              </div>
+            <div className='products'>
+                {data.products.map((product) => (
+                    <div className='product' key={product.slug}>
+                        <a href={'/product/${product.slug}'}>
+                            <img src={product.image} alt={product.name} />
+                        </a>
+                        <div className='product-info'>
+                            <a href={'/product/${product.slug}'}>
+                                <p>{product.name}</p>
+                            </a>
+                            <p><strong>${product.price}</strong></p>
+                            <button>Add to cart</button>
+                        </div>
+                    </div>
+                ))}
             </div>
-          ))}
         </div>
     );
 }
 
-export default  Products;
+export default Products;
